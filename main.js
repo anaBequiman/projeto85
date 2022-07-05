aea7f7928b60035071fad12bb9758028e536bc5e
Ana <101293323+anaBequiman@users.noreply.github.com>
2022-07-05 11:14:58 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,76 +1,70 @@
-canvas = document.getElementById("pintura");
+canvas = document.getElementById("meuCanvas");
 ctx = canvas.getContext("2d");
-var carroAltura = 115;
-var carroLargura = 65;
-var carroX = 5;
-var carroY = 225;
-backgroundImage = "parkingLot.jpg";
-carro_image = "car2.png";
+larguraImg = 600;
+alturaImg = 300;
+var imagem;
+imgX = 100;
+imgY = 100;
 function add() {
-	background_imgTag = new Image();
-	background_imgTag.onload = uploadBackground;
-	background_imgTag.src = backgroundImage
-	carro_imgTag = new Image();
-	carro_imgTag.onload = uploadCarro;
-	carro_imgTag.src = carro_image;
+	img_imgTag = new Image();
+	img_imgTag.onload = uploading;
+	img_imgTag.src = imagem;
 }
-function uploadBackground() {
-	ctx.drawImage(background_imgTag, 0, 0, canvas.width, canvas.height);
+function uploading() {
+	ctx.drawImage(img_imgTag, imgX, imgY, larguraImg, alturaImg);
 }
-function uploadCarro() {
-	ctx.drawImage(carro_imgTag, carroX, carroY, carroLargura, carroAltura);
-}
-window.addEventListener("keydown", myKeyDown);
-window.addEventListener("keydown", myKeyDown)
-function myKeyDown(e){
+window.addEventListener("keydown", my_keydown)
+function my_keydown(e){
 	keyPressed = e.keyCode;
 	console.log(keyPressed);
-		if(keyPressed == '38'){
-			up();
-			console.log("cima");
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90)){
+			//teclas do alfabeto
+			aplhabetkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla do alfabeto";
+			console.log("Tecla do alfabeto");
 		}
-		if(keyPressed == '40'){
-			down();
-			console.log("baixo");
+		else if((keyPressed >=48 && keyPressed <=57)){
+			//tecla dos números
+			numberkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla dos números";
+			console.log("Tecla numérica");
 		}
-		if(keyPressed == '37'){
-			left();
-			console.log("esquerda");
+		else if((keyPressed >=37 && keyPressed <=40)){
+			//tecla direcional
+			arrowkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla direcional";
+			console.log("Tecla direcional");
 		}
-		if(keyPressed == '39'){
-			right();
-			console.log("direita");
+		else if((keyPressed == 17) || (keyPressed == 18) || (keyPressed == 27)){
+			//tecla especial
+			specialkey();
+			document.getElementById("d1").innerHTML = "Você pressionou uma tecla especial";
+			console.log("Tecla especial");
+		}
+		else{
+			//outra tecla
+			otherkey();
+			document.getElementById("d1").innerHTML = "Você pressionou um símbolo ou outra tecla";
+			console.log("Outra tecla");
 		}
 }
-function up(){
-	if(carroY >= 0){
-		carroY = carroY - 10;
-		console.log("Tecla para cima foi pressionada, x = " + carroX + " / y = carroY");
-		uploadBackground();
-		uploadCarro();
-	}
+function aplhabetkey(){
+	imagem = "TECLA ALFABÉTICA.png";
+	add();
 }
-function down(){
-	if(carroY <= 300){
-		carroY = carroY + 10;
-		console.log("Tecla para baixo foi pressionada, x = " + carroX + " / y = carroY");
-		uploadBackground();
-		uploadCarro();
-	}
+function numberkey(){
+	imagem = "TECLA NUMÉRICA.png";
+	add();
 }
-function left(){
-	if(carroX > 0){
-		carroX = carroX - 10;
-		console.log("Tecla para esquerda foi pressionada, x = " + carroX + " / y = carroY");
-		uploadBackground();
-		uploadCarro();
-	}
+function arrowkey(){
+	imagem = "TECLA DIRECIONAL.png";
+	add();
 }
-function right(){
-	if(carroX <= 700){
-		carroX = carroX + 10;
-		console.log("Tecla para direita foi pressionada, x = " + carroX + " / y = carroY");
-		uploadBackground();
-		uploadCarro();
-	}
+function specialkey(){	
+	imagem = "TECLA ESPECIAL.png";
+	add();
+}
+function otherkey(){
+	imagem = "OUTRA TECLA.png";
+	add();
 }
